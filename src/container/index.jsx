852eb502ddd1e7639adeb04895c5d0b5adddbd46
Apple <apple@AppledeMacBook-Pro.local>
2019-01-23 13:29:30 +0800

    修改了精华和置顶贴样式

--- src/container/index.jsx
+++ src/container/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Footer } from '../components/Footer'
 import { apiGet } from '../utils/apiRequest'
+import dayjs from 'dayjs'
 
 class Post extends Component {
     constructor(props){
@@ -12,10 +13,24 @@ class Post extends Component {
             last_reply_at, reply_count, tab, title,
             top, visit_count, id, good } = this.props
 
+        let tabMap = {
+            share: '分享',
+            ask: '问答',
+            job: '招聘'
+        }
+
         return (
             <div className='strand'>
                 <img className='strand_avatar' src={author.avatar_url}/>
-                <div className='strand_title'><span>{title}</span></div>
+                <div className='strand_title'>{top ? <span className='strand_top'>置顶</span> : good ? 
+        <span className='strand_good'>精华</span> : <span className='strand_tab'>{tabMap[tab]}</span>}<span>{title}</span></div>
+                <div className='strand_s'>
+                    <div className='strand_info'>
+                        <div className='strand_visit'>浏览: {visit_count}</div>
+                        <div className='strand_reply'>回复: {reply_count}</div>
+                        <div className='strand_create'>发布于: {create_at}</div>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -28,7 +43,7 @@ class Main extends Component {
         this.state = {
             page: 1,
             tab: 'all',
-            limit: 10,
+            limit: 15,
             resdata: [],
         }
 
@@ -36,26 +51,49 @@ class Main extends Component {
             let { page, tab, limit } = this.state
             let res = await apiGet('https://www.vue-js.com/api/v1/topics', { page, tab, limit})
             if( res.status === 200 ){
-                this.setState({
-                    resdata: res.data.data
-                })
+                this.setState((prevState) => ({
+                    resdata: [...prevState.resdata, ...res.data.data]
+                }))
             }
         }
+
+        this.formatTime = (time) => {
+            let dateObj = dayjs(time)
+            
+            return `${dateObj.$y}-${dateObj.$M + 1}-${dateObj.$D}`
+        }
+
     }
 
     componentWillMount(){
         this.getData()
     }
 
+    componentDidMount(){
+        window.addEventListener('scroll', () => {
+            let height = document.body.scrollTop || document.documentElement.scrollTop
+            if(( height / this.refs.post.scrollHeight ) > 0.48 ){
+                this.setState((prevState) => ({
+                    page: prevState.page + 1
+                }))
+                this.getData()
+            }
+        }
+        )
+    }
+
     render(){
         let res = this.state.resdata
-        console.log(res)
+        console.log(this.state.resdata)
         return (
-            <div className='container'>
-                <div className='post_container'>
+            <div className='container' onScroll={this.handleScroll}>
+                <div className='post_container' ref='post'>
                     { res.map( strand => 
-                    <Post key={strand.id} author={strand.author} title={strand.title}>
-                    </Post> ) }
+                    <Post key={strand.id} author={strand.author} 
+                    title={strand.title} create_at={this.formatTime(dayjs(strand.create_at))}
+                    reply_count={strand.reply_count} visit_count={strand.visit_count}
+                    tab={strand.tab} good={strand.good} top={strand.top}  />
+                    ) }
                 </div>
                 <Footer currentCategory='首页'></Footer>
             </div>
